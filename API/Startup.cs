d8ab6160cb90abdc9926c43f031e14cca8612528
Rishav <progammingwithrishav@gmail.com>
2020-06-01 00:48:18 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,14 +18,18 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //Here ordering doesnot matter.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   
+            //
+            services.AddScoped<IProductRepository,ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Here Ordering matters.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
