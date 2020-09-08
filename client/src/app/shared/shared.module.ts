9c15b3240ae61161cf73a85ed5c0333123609e99
Rishav <progammingwithrishav@gmail.com>
2020-09-09 00:06:04 +0530

    End of section 14

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination'; // It is different than Angular 8
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel'; // It is different than Angular 8
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component'; // It is different than Angular 8
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -16,7 +17,8 @@ import { CarouselModule } from 'ngx-bootstrap/carousel'; // It is different than
   exports: [PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
-    CarouselModule
+    CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
